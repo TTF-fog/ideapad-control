590a010f5e81675f8c65920ac76ad3c869b6ca95
TTF-fog <adh.ishaan10@gmail.com>
2024-12-16 21:56:07 +0530

    Switching works!!!

--- example/simple/main.cpp
+++ example/simple/main.cpp
@@ -1,43 +1,65 @@
 #include <iostream>
 #include <memory>
+#include <regex>
 #include <tray.hpp>
+
 enum MODES
 {
     COOLING = 0,
-    BATTERY = 1,
-    PERFORMANCE = 2,
+    PERFORMANCE = 1,
+    BATTERY = 2,
 };
+
 void handle_others(std::vector<std::shared_ptr<Tray::Button>> buttons, MODES mode)
-{switch (mode)
+{
+    const std::regex pattern("\\[\\*\\]");
+    switch (mode)
     {
         case MODES::COOLING:
-            std::cout << "COOLING" << std::endl;
-            buttons[1]->setDisabled(true);
-            buttons[2]->setDisabled(true);
             buttons[0]->setText("Intelligent Cooling [*]");
+            for (int i = 0 ; i < static_cast<int>(buttons.size()); i++)
+            {
+                if (i == MODES::COOLING) {continue;}
+                buttons[i] ->setText( std::regex_replace(buttons[i]->getText(), pattern, " "));
+            }
             break;
-        case MODES::BATTERY:
-            std::cout << "BATTERY" << std::endl;
-            break;
-        case MODES::PERFORMANCE:
-            std::cout << "PERFORMANCE" << std::endl;
-            break;
-    }
+        case BATTERY:
+            buttons[MODES::BATTERY]->setText("Battery Saver [*]");
+            for (int i = 0 ; i < static_cast<int>(buttons.size()); i++)
+            {
+                if (i == MODES::BATTERY) {continue;}
+                buttons[i] ->setText( std::regex_replace(buttons[i]->getText(), pattern, " "));
+            }
+                break;
+            case MODES::PERFORMANCE:
+                buttons[MODES::PERFORMANCE]->setText("Extreme Performance [*]");
+                for (int i = 0 ; i < static_cast<int>(buttons.size()); i++)
+                {
+                    if (i == MODES::PERFORMANCE) {continue;}
+                    buttons[i] ->setText( std::regex_replace(buttons[i]->getText(), pattern, " "));
+                }
+                break;
+        }
 }
 
 int main()
 {
     std::vector<std::shared_ptr<Tray::Button>> buttons;
-    Tray::Tray tray("test", "icon.svg");
-    std::shared_ptr<Tray::Submenu> submenu= tray.addEntry(Tray::Submenu("Test Submenu"));
-    auto m = submenu->addEntry(Tray::Button("Intelligent Cooling",[&] {
-        handle_others(buttons, MODES::COOLING);;
+    Tray::Tray tray("Ideapad Control", "icon.svg");
+    tray.addEntry(Tray::Label("Ideapad Control"))->setDisabled(false);
+    std::shared_ptr<Tray::Submenu> const submenu= tray.addEntry(Tray::Submenu("Power Modes"));
+    auto cool_button = submenu->addEntry(Tray::Button("Intelligent Cooling",[] {
+        handle_others(buttons, MODES::COOLING);
     }));
-    buttons.push_back(m);
-    auto t = submenu->addEntry(Tray::Button("Extreme Performance"));
-    buttons.push_back(t);
-    auto n = submenu->addEntry(Tray::Button("Battery Saver"));
-    buttons.push_back(n);
+    buttons.push_back(cool_button);
+    auto perf_button = submenu->addEntry(Tray::Button("Extreme Performance", [] {
+        handle_others(buttons, MODES::PERFORMANCE);
+    }));
+    buttons.push_back(perf_button);
+    auto batt_button = submenu->addEntry(Tray::Button("Battery Saver",[] {
+        handle_others(buttons, MODES::BATTERY);
+    }));
+    buttons.push_back(batt_button);
     tray.run();
 
     return 0;
